e6515e725b9c6dcdaa2b7dd5cffaae66082376e0
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-28 12:00:48 -0500

    Switched to pip3 and improved error catching and logs

--- src/kernel/system/jupyter.ts
+++ src/kernel/system/jupyter.ts
@@ -4,20 +4,38 @@ import { promiseExec } from '../../shared/system/process';
  * Get the version of the kernel gateway on the host. This serves as a way of verifying the gateway is operational
  */
 export const getGatewayVersion = async (): Promise<string | null> => {
-  const { stdout, stderr } = await promiseExec('jupyter kernelgateway --version');
+  let output: {
+    stdout: string;
+    stderr: string;
+  } | null = null;
 
-  if (stderr) {
+  try {
+    output = await promiseExec('jupyter kernelgateway --version');
+  } catch (error) {
+    console.error(error);
     return null;
   }
 
-  return stdout;
+  if (output?.stderr) {
+    console.log('stderr', output.stderr);
+    return null;
+  }
+
+  return output?.stdout ?? null;
 };
 
 /**
  * Try to install the kernel gateway and then return the version
  */
 export const installGateway = async (): Promise<string | null> => {
-  await promiseExec('pip install jupyter_kernel_gateway');
+  try {
+    const { stderr, stdout } = await promiseExec('pip3 install jupyter_kernel_gateway');
+
+    console.log('Install output', { stderr, stdout });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 
   return getGatewayVersion();
 };
