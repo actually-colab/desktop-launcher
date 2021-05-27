290289ceb6c431f7d566c2e6a1c4cc9bb6fc3636
Jeff Taylor-Chang <jeff@jefftc.com>
2021-05-27 11:55:50 -0400

    Allowed wildcard subdomains

--- src/shared/constants/client.ts
+++ src/shared/constants/client.ts
@@ -1,7 +1,7 @@
 export const ALLOWED_ORIGIN =
   process.env.NODE_ENV === 'development'
-    ? /^http:\/\/localhost:4000$|^https:\/\/staging\.actuallycolab\.org$/
-    : /^https:\/\/staging\.actuallycolab\.org$|^https:\/\/app\.actuallycolab\.org$/;
+    ? /^http:\/\/localhost:4000$|^https:\/\/.+\.actuallycolab\.org$/
+    : /^https:\/\/.+\.actuallycolab\.org$/;
 
 export const ALLOWED_ORIGIN_PRETTY =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://app.actuallycolab.org';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://*.actuallycolab.org';
